c9238949bc9c756e7cf52238819b14a5a7799f78
sence <sence@sence-pc>
2013-12-03 17:17:22 +0800

    调整类包结构

---
+++ src/main/java/com/wechat/menu/pojo/MenuBasic.java
@@ -0,0 +1,16 @@
+package com.wechat.menu.pojo;
+
+public class MenuBasic {
+	
+	  private String name;  
+	  
+	    public String getName() {  
+	        return name;  
+	    }  
+	  
+	    public void setName(String name) {  
+	        this.name = name;  
+	    }  
+
+
+}
---
+++ src/main/java/com/wechat/menu/pojo/TokenPojo.java
@@ -0,0 +1,26 @@
+package com.wechat.menu.pojo;
+
+public class TokenPojo {
+	
+	// 获取到的凭证  
+    private String token;  
+    // 凭证有效时间，单位：秒  
+    private int expiresIn;  
+  
+    public String getToken() {  
+        return token;  
+    }  
+  
+    public void setToken(String token) {  
+        this.token = token;  
+    }  
+  
+    public int getExpiresIn() {  
+        return expiresIn;  
+    }  
+  
+    public void setExpiresIn(int expiresIn) {  
+        this.expiresIn = expiresIn;  
+    }  
+
+}
