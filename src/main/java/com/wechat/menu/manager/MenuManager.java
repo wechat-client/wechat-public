061a1e6ff1c542c9098acab28303cd71a82dfa2c
sence <sence@sence-pc>
2013-12-04 13:08:00 +0800

    添加对菜单操作的测试用例，删除MenuManager main方法

--- src/main/java/com/wechat/menu/manager/MenuManager.java
+++ src/main/java/com/wechat/menu/manager/MenuManager.java
@@ -4,6 +4,8 @@ package com.wechat.menu.manager;
 import java.util.ArrayList;
 import java.util.List;
 
+import net.sf.json.JSONObject;
+
 import com.wechat.menu.pojo.*;
 import com.wechat.wechatutil.MainUtil;
 import com.wechat.wechatutil.MenuUtil;
@@ -11,21 +13,11 @@ import com.wechat.wechatutil.MenuUtil;
 
 public class MenuManager {
 	
-    public static void main(String[] args) {  
-        // 第三方用户唯一凭证  
-        String appId = "wx136d668a243902c0";  
-        // 第三方用户唯一凭证密钥  
-        String appSecret = "1d557540724ec17578433e3040ebacf3";  
-  
-        // 调用接口获取access_token  
-        TokenPojo at = MainUtil.getAccessToken(appId, appSecret);  
-  
-        if (null != at) {  
-            // 调用接口创建菜单  
-            int result = MenuUtil.createMenu(getMenu(), at.getToken());  
-        }  
-    }  
-  
+	// 菜单创建（POST） 限100（次/天）  
+    public static final String MENU_CREATE_URL = "https://api.weixin.qq.com/cgi-bin/menu/create?access_token=ACCESS_TOKEN";  
+    // 菜单创建（POST） 限100（次/天）  
+    public static final String MENU_DELETE_URL = "https://api.weixin.qq.com/cgi-bin/menu/delete?access_token=ACCESS_TOKEN";  
+    
     /** 
      * 组装菜单数据 
      * @return 
@@ -45,7 +37,7 @@ public class MenuManager {
     	
     	Menu pM_1_sub_2 = new Menu();
     	pM_1_sub_2.setName("非姐V付");
-    	pM_1_sub_2.setType(MenuUtil.MENU_TYPE_VIEW);
+    	pM_1_sub_2.setType(MenuUtil.MENU_TYPE_CLICK);
     	pM_1_sub_2.setKey(MenuUtil.KEY_V_PAY);
     	
     	//添加到一级菜单
@@ -71,5 +63,71 @@ public class MenuManager {
     	
     	return menus;
     }  
+    
+    
+       
+    /** 
+     * 创建菜单 
+     *  
+     * @param menu 菜单实例 
+     * @param accessToken 有效的access_token 
+     * @return 0表示成功，其他值表示失败 
+     */  
+    public static int createMenu(List<Menu> menus, String accessToken) {  
+        int result = 0;  
+      
+        // 拼装创建菜单的url  
+        String url = MENU_CREATE_URL.replace("ACCESS_TOKEN", accessToken);  
+        // 将菜单对象转换成json字符串  
+        //String jsonMenu = JSONObject.fromObject(button).toString().trim();  
+        JSONObject jsonMenu = new JSONObject();
+        jsonMenu.put("button", menus);
+        
+        System.out.println(accessToken);
+        
+        System.out.println(jsonMenu);
+ 
+        // 调用接口创建菜单  
+        JSONObject jsonObject = MainUtil.httpRequest(url, "POST", jsonMenu.toString());  
+      
+        if (null != jsonObject) {  
+            if (0 != jsonObject.getInt("errcode")) {  
+                result = jsonObject.getInt("errcode");  
+                //log.error("创建菜单失败 errcode:{} errmsg:{}", jsonObject.getInt("errcode"), jsonObject.getString("errmsg"));  
+            }  
+        }  
+      
+        return result;  
+    }
+    
+    
+    /**
+     * 
+     * @param accessToken
+     * @return 
+     */
+    public static int deleteMenu(String accessToken) {
+    	
+    	 String url = MENU_DELETE_URL.replace("ACCESS_TOKEN", accessToken);  
+    	
+    	 // 调用接口创建菜单  
+         JSONObject jsonObject = MainUtil.httpRequest(url, "GET",null);  
+         
+         int result = 0;  
+         
+         if (null != jsonObject) {  
+             if (0 != jsonObject.getInt("errcode")) {  
+                 result = jsonObject.getInt("errcode");  
+                 //log.error("创建菜单失败 errcode:{} errmsg:{}", jsonObject.getInt("errcode"), jsonObject.getString("errmsg"));  
+             }  
+         }  
+       
+         return result;  
+         
+    	
+    }
+    
+    
+    
 
 }
--- src/test/java/com/wechat/menu/MenuManagerTest.java
+++ src/test/java/com/wechat/menu/MenuManagerTest.java
@@ -4,13 +4,25 @@ import java.util.List;
 
 import net.sf.json.JSONObject;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import com.wechat.menu.manager.MenuManager;
 import com.wechat.menu.pojo.Menu;
+import com.wechat.menu.pojo.TokenPojo;
+import com.wechat.wechatutil.MainUtil;
 
 
 public class MenuManagerTest {
+	
+	
+	  // 第三方用户唯一凭证  
+    String appId = "wx136d668a243902c0";  
+    // 第三方用户唯一凭证密钥  
+    String appSecret = "1d557540724ec17578433e3040ebacf3";  
+
+    
+    
 
 	/**
 	 *测试menu生成的json是否符合需求
@@ -22,5 +34,31 @@ public class MenuManagerTest {
 		jsonMenu.put("button", menus);
 		System.out.println(jsonMenu.toString());
 	}
+	/**
+	 * 测试删除menu
+	 */
+	@Test
+	public void testDeleteMenu(){
+
+        // 调用接口获取access_token  
+        TokenPojo at = MainUtil.getAccessToken(appId, appSecret);  
+        int result = MenuManager.deleteMenu(at.getToken());
+        Assert.assertEquals(0, result);
+       
+	}
 	
+	/**
+	 * 测试添加menu
+	 */
+	@Test
+	public void testCreateMenu(){
+        // 调用接口获取access_token  
+        TokenPojo at = MainUtil.getAccessToken(appId, appSecret);  
+        //创建menu
+        List<Menu> menus = MenuManager.getMenu();
+        
+        int result = MenuManager.createMenu(menus, at.getToken());
+        Assert.assertEquals(0, result);
+       
+	}
 }
