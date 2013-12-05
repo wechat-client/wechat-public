fcc073d40d7a4b2199245158b2ffa36c801d0320
sence <lovesenceqi@gmail.com>
2013-12-05 12:47:10 +0800

    change message

--- src/main/java/com/wechat/common/utils/MenuUtil.java
+++ src/main/java/com/wechat/common/utils/MenuUtil.java
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-package com.wechat.common.utils;
-
-
-public class MenuUtil {
-	
-	/*menu的类型为click类型，有key值*/
-	public static final String MENU_TYPE_CLICK = "click";
-	/*menu类型为view类型，有Url*/
-	public static final String MENU_TYPE_VIEW = "view";
-	
-	//非姐V支付key code
-	public static final String KEY_V_PAY = "v_pay";
-	//私房菜key code
-	public static final String KEY_PERSIONAL_KITCHEN = "personal_kitchen";
-	//温灸
-	public static final String KEY_WARM_MOXIBUSTION = "warm_moxibustion";
-	
-	
-}
-=======
 package com.wechat.common.utils;
 
 
@@ -43,4 +22,4 @@ public class MenuUtil {
     
 	
 }
->>>>>>> 1d30d1e5ce4a8d1a2d83bc5f96af78e2cd437011
+
--- src/main/java/com/wechat/message/controller/MessageController.java
+++ src/main/java/com/wechat/message/controller/MessageController.java
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-package com.wechat.message.controller;
-
-import java.io.PrintWriter;
-
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.wechat.common.utils.SignUtil;
-
-
-@Controller
-public class MessageController {
-
-	@RequestMapping("/wechatClient")
-	public @ResponseBody String ensureWechatCallBack(String signature,String timestamp,String nonce,String echostr){
-		
-        // 通过检验signature对请求进行校验，若校验成功则原样返回echostr，表示接入成功，否则接入失败  
-        if (SignUtil.checkSignature(signature, timestamp, nonce)) {  
-           return echostr;
-        }
-        return "error";
-        
-	}
-	
-}
-=======
 package com.wechat.message.controller;
 
 import java.io.PrintWriter;
@@ -50,4 +22,4 @@ public class MessageController {
 	}
 	
 }
->>>>>>> 1d30d1e5ce4a8d1a2d83bc5f96af78e2cd437011
+
